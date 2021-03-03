b4441cfdaa657117829cc0d37080e96bc63c81ac
prince-mahto <prince.mahto@jocata.com>
2021-03-03 17:07:19 +0530

    MH-667 next commit

--- src/com/jocata/Demo2.java
+++ src/com/jocata/Demo2.java
@@ -3,9 +3,11 @@ package com.jocata;
 public class Demo2 {
 
 	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("welcome to eclips git concept deeply"+a+b);
 		System.out.println("welcome to eclips git concept deeply");
-		System.out.println("welcome to eclips git concept deeply");
-		System.out.println("welcome to eclips git concept deeply");
+		
 	}
 
 }
