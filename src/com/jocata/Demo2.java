f31d9d208aa74f8c6cb3dc999da916967b5041f4
prince-mahto <prince.mahto@jocata.com>
2021-03-03 17:09:26 +0530

    mh-123 commit again

--- src/com/jocata/Demo2.java
+++ src/com/jocata/Demo2.java
@@ -5,7 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
-		System.out.println("welcome to eclips git concept deeply"+a+b);
+		System.out.println("welcome to eclips git concept deeply"+(a+b));
 		System.out.println("welcome to eclips git concept deeply");
 		
 	}
